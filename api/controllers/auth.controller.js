d71adad0fb2cd3374bec4582f294b991d73b6a8c
prrtek <prrtek@outlook.com>
2024-03-04 13:04:19 +0530

    Add login functionality to auth controller

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js";
-
+import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
 const register = async (req, res) => {
   try {
     const userExists = await User.findOne({ email: req.body.email });
@@ -19,9 +20,28 @@ const register = async (req, res) => {
   }
 };
 
-const login = (req, res) => {
-  res.send("Login route");
-  console.log(req.body);
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(400).json("Please provide an email and password");
+  }
+  try {
+    const userExists = await User.findOne({ email: req.body.email });
+    if (!userExists) return res.status(400).json("User does not exist");
+
+    const validPassword = await userExists.matchPasswords(password);
+    if (!validPassword) return res.status(400).json("Invalid credentials");
+
+    const token = jwt.sign({ id: userExists._id }, process.env.JWT_SECRET, {
+      expiresIn: "30d",
+    });
+    const options = {
+      httpOnly: true,
+    };
+    res.status(200).cookie("access_token", token, options).json("Logged in");
+  } catch (err) {
+    res.status(500).json(err);
+  }
 };
 
 export { register, login };
